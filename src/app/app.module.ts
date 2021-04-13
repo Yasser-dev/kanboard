d7e6c0be71ebcb58cc58bda769c2611ecbf7d3e2
Yasser Ehab <56257332+Yasser-dev@users.noreply.github.com>
2021-04-13 19:39:01 +0800

    navigation bar, dark & light modes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
+import { SharedModule } from './shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
